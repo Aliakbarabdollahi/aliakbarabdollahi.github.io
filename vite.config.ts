d9cc5face49f3964bb7c5debc59da23b112c7bb2
Akiakbar <Aliakbarabdollahi@outlook.com>
2025-12-13 19:46:56 +0330

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // تغییر به نسبی
+  base:"/aliakbarabdollahi.github.io/",
+  server:{
+    open: true,
+    port:3001
+  }
 })
